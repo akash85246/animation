4ae2a7896f6dfc581a42eb1ea3fa18e004a1e059
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-03 01:28:11 +0530

    Added on crash

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function moveBackground() {
     updateBackground();
 }
 
-moveBackground(); 
+// 
 
 // alias ship movement
 
@@ -70,9 +70,69 @@ function moveEnemyShipDownWithRandomDelay(shipId) {
         moveEnemyShipDown(shipId);
     }, Math.random() * 20500); 
 }
+function enemyMove(){
 moveEnemyShipDownWithRandomDelay('eship1');
 moveEnemyShipDownWithRandomDelay('eship2');
 moveEnemyShipDownWithRandomDelay('eship3');
 moveEnemyShipDownWithRandomDelay('eship4');
 moveEnemyShipDownWithRandomDelay('eship5');
-//for collison
+}
+
+let bool=true;
+
+document.querySelector(".b1").addEventListener("click",function(){
+    if(bool)
+    {
+    enemyMove();
+    moveBackground(); 
+    document.querySelector(".b1").innerHTML="Pause";
+    score();
+    bool=false;
+    }
+})
+var n=0;
+function score(){
+setInterval(() => {
+    document.getElementById("score").innerHTML=n;
+    n=n+1;
+
+    var eship1_left=Math.abs(document.querySelector("#eship1").getBoundingClientRect().left);
+    var eship1_right=Math.abs(document.querySelector("#eship1").getBoundingClientRect().right);
+    var eship1_top=Math.abs(document.querySelector("#eship1").getBoundingClientRect().top);
+    var eship1_bottom=Math.abs(document.querySelector("#eship1").getBoundingClientRect().bottom);
+    
+    var eship2_left=Math.abs(document.querySelector("#eship2").getBoundingClientRect().left);
+    var eship2_right=Math.abs(document.querySelector("#eship2").getBoundingClientRect().right);
+    var eship2_top=Math.abs(document.querySelector("#eship2").getBoundingClientRect().top);
+    var eship2_bottom=Math.abs(document.querySelector("#eship2").getBoundingClientRect().bottom);
+
+    var eship3_left=Math.abs(document.querySelector("#eship3").getBoundingClientRect().left);
+    var eship3_right=Math.abs(document.querySelector("#eship3").getBoundingClientRect().right);
+    var eship3_top=Math.abs(document.querySelector("#eship3").getBoundingClientRect().top);
+    var eship3_bottom=Math.abs(document.querySelector("#eship3").getBoundingClientRect().bottom);
+
+    var eship4_left=Math.abs(document.querySelector("#eship4").getBoundingClientRect().left);
+    var eship4_right=Math.abs(document.querySelector("#eship4").getBoundingClientRect().right);
+    var eship4_top=Math.abs(document.querySelector("#eship4").getBoundingClientRect().top);
+    var eship4_bottom=Math.abs(document.querySelector("#eship4").getBoundingClientRect().bottom);
+
+    var eship5_left=Math.abs(document.querySelector("#eship5").getBoundingClientRect().left);
+    var eship5_right=Math.abs(document.querySelector("#eship5").getBoundingClientRect().right);
+    var eship5_top=Math.abs(document.querySelector("#eship5").getBoundingClientRect().top);
+    var eship5_bottom=Math.abs(document.querySelector("#eship5").getBoundingClientRect().bottom);
+
+    var myShip_left=Math.abs(document.querySelector("#alia").getBoundingClientRect().left);
+    var myShip_right=Math.abs(document.querySelector("#alia").getBoundingClientRect().right);
+    var myShip_top=Math.abs(document.querySelector("#alia").getBoundingClientRect().top);
+    var myShip_bottom=Math.abs(document.querySelector("#alia").getBoundingClientRect().bottom);
+
+    if(((eship1_left<myShip_left && myShip_left<eship1_right)||(eship1_left<myShip_right && myShip_right<eship1_right)) && ((eship1_top<myShip_top && myShip_top<eship1_bottom)||(eship1_top<myShip_bottom && myShip_bottom<eship1_bottom)))
+    {
+        setTimeout(() => {
+            alert("Game Over");
+        })
+        location.reload();
+    }
+}, 3000);
+}
+
