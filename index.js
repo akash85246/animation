52f79e8febda414498410e584b564b6a5f4d3936
Akash <sgc.akash.22oct.11sci@gmail.com>
2023-10-02 13:02:42 +0530

    Added movement to my ship

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// for looping back ground
 function moveBackground() {
     const background = document.querySelector(".screen>.game-container>.background");
     var position = 0;
@@ -13,3 +14,36 @@ function moveBackground() {
 
 moveBackground(); 
 
+// alias ship movement
+
+const spaceship = document.querySelector('#alia');
+const container = document.querySelector('.background');
+
+
+var spaceshipX = container.clientWidth / 2; 
+var spaceshipY = container.clientHeight / 2;
+
+const step = 10;
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            spaceshipY -= step;
+            break;
+        case 'ArrowDown':
+            spaceshipY += step;
+            break;
+        case 'ArrowLeft':
+            spaceshipX -= step;
+            break;
+        case 'ArrowRight':
+            spaceshipX += step;
+            break;
+    }
+
+    spaceshipX = Math.max(0, Math.min(container.clientWidth - spaceship.clientWidth, spaceshipX));
+    spaceshipY = Math.max(0, Math.min(container.clientHeight - spaceship.clientHeight, spaceshipY));
+
+    spaceship.style.left = spaceshipX + 'px';
+    spaceship.style.top = spaceshipY + 'px';
+});
